9b53fba6c2285550f40f03380c247d8f33d02d03
Matheus Cristhyan <matheuscristhyan.2@gmail.com>
2024-04-21 23:00:45 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ app.listen({
   host: "0.0.0.0",
   port: env.PORT
 }).then(() => {
-  console.log(`HTTP Server is Running on Port ${env.PORT}`);
+  console.log(`HTTP Server is Running on PORT ${env.PORT}`);
 })
